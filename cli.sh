ef8d84926782818f9c07ee5d5a395fe6efaed163
Jon Goodger <jonno.is@gmail.com>
2025-07-07 23:06:49 +0100

    Use Jolt Scripts for commands

--- cli.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker-compose exec interface php srdb.cli.php -h db -u root -p joltdbpass -n database $@
--- export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker-compose exec db mysqldump -u root -pjoltdbpass database > ./db-output/output.sql
